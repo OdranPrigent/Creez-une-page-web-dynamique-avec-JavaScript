92927a7dc5128f6343d6e884e3d6103526f9f0d0
OdranPrigent <odran.prigent@hotmail.fr>
2024-05-20 17:33:43 +0200

    js login administrateur

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
@@ -1,9 +1,44 @@
 
 const form = document.getElementById('connect');
+let admin = false;
 
 function connection(e) {
     e.preventDefault();
-  console.log(form.password.value);
+  const m = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/.test(form.mail.value);
+  if (m)
+    if (form.password.value != ''){
+      fetch("http://localhost:5678/api/users/login", 
+      {method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },body: JSON.stringify({"email": form.mail.value,
+        "password": form.password.value})})
+      .then(reponse => {
+        if(reponse.status!=200){
+          document.getElementById('mdp').innerHTML = "Nom d'utilisateur ou email invalide.";
+          admin = false;
+        }
+        else{
+          admin = true;
+          return reponse.json()
+        }
+          })
+      .then(reponse => {testReponse(reponse)})
+      .catch ((error) => {console.log(error)})
+    }
+    else
+      document.getElementById('mdp').innerHTML = "champ mdp vide";
+  else
+    document.getElementById('mdp').innerHTML = "e-mail non conforme";
 }
 
 form.addEventListener("submit", connection);
+
+function testReponse(reponse){
+  if (admin){
+    document.getElementById('mdp').innerHTML = "Connexion...";
+
+    sessionStorage.setItem("tokenSophieBluel", reponse.token);
+    console.log(sessionStorage.getItem("tokenSophieBluel"));
+  }
+}
